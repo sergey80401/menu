50a0c0b873d5f1696ccdc626086341da6c5f3432
skryuchkov <skryuchkov@gemsdev.com>
2025-02-28 14:43:18 +0600

    Simple grpc  service for menu has been added

---
+++ serever/GrpcServiceApp/Services/MenuViewerService.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Core;
+
+namespace GrpcServiceApp.Services;
+
+public class MenuViewerService : MenuViewer.MenuViewerBase
+{
+    private readonly ILogger<MenuViewerService> _logger;
+
+    public MenuViewerService(ILogger<MenuViewerService> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task<Reply> ShowMenu(Request request, ServerCallContext context)
+    {
+        return Task.FromResult(new Reply()
+        {
+            Id = "1",
+            Title = "123"
+        });
+    }
+}
